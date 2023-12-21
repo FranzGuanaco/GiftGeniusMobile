fff1e633ce135d2e25f081adca59a7550b19aa7f
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-12-22 00:49:27 +0100

    new change in package

---
+++ Firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import analytics from '@react-native-firebase/analytics';
+
+// Votre configuration Firebase
+const firebaseConfig = {
+  apiKey: "Votre clé API",
+  authDomain: "Votre domaine d'authentification",
+  projectId: "Votre ID de projet",
+  storageBucket: "Votre bucket de stockage",
+  messagingSenderId: "Votre ID d'envoi de messages",
+  appId: "Votre ID d'application",
+  measurementId: "Votre ID de mesure"
+};
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+export { firestore, auth, analytics };
